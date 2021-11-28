4ca05bb92e2d7f7e43a33482e7e0277b8530dee4
Todd Wood <WoodT@canbyoregon.gov>
2021-11-27 22:43:16 -0800

    first commit from Todd

---
+++ js/main.js
@@ -0,0 +1,86 @@
+const createElemWithText = (HTMLElem = "p", textContent = "", className ) => {
+
+    const element = document.createElement(HTMLElem);
+ 
+     element.textContent = textContent;
+ 
+     if (className) element.className = className;
+ 
+      return element;
+ 
+ }
+ 
+ const createSelectOptions = (users) => {
+  
+     if (!users) {
+ 
+         return undefined;
+ 
+     }
+ 
+     const options = [];
+ 
+     for(let i = 0; i < users.length; i++) {
+ 
+        options[i] = new Option( users[i].name, users[i].id);           
+ 
+     }
+     return options;
+ 
+ }
+ 
+ const toggleCommentSection = (postID) => {
+ 
+      if (!postID) { 
+         return undefined; 
+     }
+     let section = document.getElementById(postID);
+     console.log(section);
+
+        if(!section){ 
+        return null; 
+        }
+    
+     section.classList.toggle("hide");
+     return section;
+ 
+ }
+ 
+ const toggleCommentButton = (postID) => {
+ 
+     if (!postID){
+ 
+         return undefined; 
+     }
+ 
+     const commentButton = document.postID;
+ 
+     if (!commentButton){
+ 
+         return null;
+ 
+     }
+ 
+     commentButton.textContent = commentButton.textContent === "Show Comments" ? "Hide Comments":"Show Comments";
+ 
+     return commentButton;
+ 
+ }
+  const deleteChildElements = (parentElement) => {
+ 
+     if ( typeof(parentElement) != "HTML" ) {
+ 
+         return undefined;  
+     }
+ 
+     let child = parentElement.lastElementChild;
+ 
+     while(child) {
+ 
+         parentElement.removeChild(child);
+          child = parentElement.lastElementChild;
+ 
+     }
+      return parentElement;
+ 
+  }
