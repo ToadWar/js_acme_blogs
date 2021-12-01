4f73cca886131167a31bbcc1bf7a7bab6013cf4b
Todd Wood <woodtm@comcast.net>
2021-12-01 12:04:07 -0800

    Added Comments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+/*
+Author: Todd M. Wood
+Project: Final INF 651
+Date: 12/1/2021
+Description: This code creates posts and functionality for an employee database.  Allows the user to select an employee and see the posts. 
+*/
+
+//Creates an HTML element with the given parameters.  Returns the Element.
 const createElemWithText = (HTMLElem = 'p', textContent = '', className ) => {
     const element = document.createElement(HTMLElem);
  
@@ -8,6 +16,7 @@ const createElemWithText = (HTMLElem = 'p', textContent = '', className ) => {
      return element;
  }
  
+ // Creates an options list with the given users parameter.  Returns the options element
  const createSelectOptions = (users) => {
      if (!users)  return ;
  
@@ -20,6 +29,7 @@ const createElemWithText = (HTMLElem = 'p', textContent = '', className ) => {
       return options;
  }
  
+ //Hides or shows the comment section by adding or remove the hide class.  Returns the section
  const toggleCommentSection = (postID) => {
     if (!postID) return;
     
@@ -32,6 +42,7 @@ const createElemWithText = (HTMLElem = 'p', textContent = '', className ) => {
     return section;
 }
 
+//Changes the text on the comment button based on it's current state.  Defaults to Show Comments. Returns the button
 const toggleCommentButton = (postID) => {
     if (!postID) return;
     
@@ -40,9 +51,11 @@ const toggleCommentButton = (postID) => {
     if (!commentButton) return commentButton;
     
     commentButton.textContent = commentButton.textContent === 'Show Comments' ? 'Hide Comments':'Show Comments';
+    
     return commentButton;
 }
 
+// Deletes all comments for the employee passed in. Returns the empty parent
 const deleteChildElements = (parentElement) => {
     if ( !parentElement?.tagName) return; 
 
@@ -57,6 +70,7 @@ const deleteChildElements = (parentElement) => {
     return parentElement;
 }
 
+// adds a listener for button click, calls toggle comments. Returns the buttons
 const addButtonListeners = () => {
     const main = document.querySelector('main');
     const buttons = main.querySelectorAll('button');
@@ -71,6 +85,7 @@ const addButtonListeners = () => {
     return buttons;
 }
 
+// removes the listener when the button is clicked. Returns the buttons
 const removeButtonListeners = () => {
     const main = document.querySelector('main');
     const buttons = main.querySelectorAll('button');
@@ -85,6 +100,7 @@ const removeButtonListeners = () => {
     return buttons;
 }
 
+// builds the comments fragment element based on incoming json comment data. Returns the fragment
 const createComments = (comments) => {
     if (!comments) return;
 
@@ -106,6 +122,7 @@ const createComments = (comments) => {
     return fragment;
 }
 
+// populates the menu with the users received in json format. Returns the menu
 const populateSelectMenu = (users) => {
     if (!users) return;
 
@@ -118,6 +135,7 @@ const populateSelectMenu = (users) => {
     return selectMenu;
 }
 
+// gets the users from website. Returns the data if received.  Throws an error if not. 
 const getUsers = async () => {
 
     try {
@@ -129,6 +147,7 @@ const getUsers = async () => {
     }
 }
 
+// gets the posts from website based on a user.  Returns the data if received.  Throws an error if not. 
 const getUserPosts = async (userId) => {
     if(!userId) return;
 
@@ -141,6 +160,7 @@ const getUserPosts = async (userId) => {
     }
 }
 
+// gets the user information for a specific user based on userId. Returns the data if received.  Throws an error if not. 
 const getUser = async (userId) => {
     if(!userId) return;
 
@@ -153,6 +173,7 @@ const getUser = async (userId) => {
     }
 }
 
+// gets the comments from website based on a user. Returns the data if received.  Throws an error if not. 
 const getPostComments= async (postId) => {
     if(!postId) return;
 
@@ -165,6 +186,7 @@ const getPostComments= async (postId) => {
     }
 }
 
+// builds an element with comments from a specific user.  Returns the section element. 
 const displayComments = async (postID) => {
     if (!postID) return;
 
@@ -178,6 +200,7 @@ const displayComments = async (postID) => {
     return section;    
 }
 
+// Builds the post.  Returns the post fragement. 
 const createPosts = async (posts) => {
     if (!posts) return;
 
@@ -211,6 +234,7 @@ const createPosts = async (posts) => {
     return fragment;
 }
 
+// Diplays the posts onto the screen by appending to the main element in HTML. Returns the Element. 
 const displayPosts = async (posts) => {
     const main = document.querySelector('main');
     
@@ -221,6 +245,7 @@ const displayPosts = async (posts) => {
     return element;
 }
 
+// creates an array with the comments and buttons.   Returns an array with the comments and button
 const toggleComments = (event, postID) => {
     if (!event || !postID) return;
 
@@ -230,6 +255,7 @@ const toggleComments = (event, postID) => {
     return commentAndButton; 
 }
 
+// creates an array with the listeners, parent, posts, and added listeners.  Returns the array
 const refreshPosts = async (posts) => {
     if (!posts) return;
     const main = document.querySelector('main');
@@ -239,6 +265,7 @@ const refreshPosts = async (posts) => {
     return results;
 }
 
+// Event handler selects the item on change, pulls usersID, posts results from refreshPost into an array and returns the array. 
 const selectMenuChangeEventHandler = async (event) => {
     const userId = event?.target?.value || 1;
     
@@ -248,6 +275,7 @@ const selectMenuChangeEventHandler = async (event) => {
     return arr;
 }
 
+// fires off the functions needed to gain data.  Stores teh information in an array. the app returns the needed data array
 const initPage = async () => {
     const users = await getUsers();
     const data = [users, populateSelectMenu(users)]
@@ -255,10 +283,12 @@ const initPage = async () => {
     return data;
 }
 
+// starts the needed functions, starts select event, adds the listerner for the menu.  
 const initApp = () => {
     initPage();
     const menu = document.querySelector('#selectMenu');
     menu.addEventListener('change', selectMenuChangeEventHandler, false);
 }
 
+// Waits for the DOM to fully load and begins the app. 
 document.addEventListener('DOMContentLoaded', initApp, false);
